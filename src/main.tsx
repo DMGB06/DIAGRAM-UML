3f26e4c4dfa7a93b6a59ad833bf64e4cc147e045
DMGB <2201010141@undc.edu.pe>
2026-08-12 09:06:51 -0500

    feat: replace graphite/brass tokens with the paper design system and self-hosted fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+import "@fontsource/space-grotesk/400.css";
+import "@fontsource/space-grotesk/500.css";
+import "@fontsource/space-grotesk/600.css";
+import "@fontsource/space-grotesk/700.css";
+import "@fontsource/ibm-plex-mono/400.css";
+import "@fontsource/ibm-plex-mono/500.css";
 import "@xyflow/react/dist/style.css";
 import "./styles/globals.css";
 
